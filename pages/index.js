073af475def9db330056ad9b0e8e23ed5c270cbe
Greg-CS <gregor.gr20@gmail.com>
2022-09-15 10:13:13 -0400

    Added components

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,24 @@
-import { Navbar, Footer, Hero, Breadcrumbs } from "@components/common"
+import {  Hero, Breadcrumbs } from "@components/common"
 import { CourseList } from "@components/course"
+import BaseLayout from "@components/layout/base"
 import { OrderCard } from "@components/order"
 import { EthRates, WalletBar } from "@components/web3"
-
-export default function Home() {
+import { getAllCourses } from "@content/courses/fetcher"
+export default function Home({courses}) {
   return (
-    <>
-    <div>
-      <div className="relative bg-white overflow-hidden">
-        <div className="relative max-w-7xl mx-auto px-4">
-
-          <Navbar/>
-
-          <div className="fit">
-            <Hero/>
-            <Breadcrumbs/>
-            <WalletBar/>
-            <EthRates/>
-            <CourseList/>
-            <OrderCard/>
-          </div>
-          <Footer/>
-        </div>
-      </div>
-    </div>    
-    </>
+      <BaseLayout>
+        <Hero/>
+        {JSON.stringify(courses)}
+        <CourseList/>
+      </BaseLayout>
   )
+}
+
+export function getStaticProps() {
+  const { data } = getAllCourses();
+  return {
+    props: {
+      courses: data
+    }
+  }
 }
